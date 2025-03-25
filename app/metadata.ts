7db8a687784c5bd7b7ebc6ed68a90d9a79ff4c15
Chi Joshua <chijosh2010@hotmail.com>
2025-03-25 02:33:34 +0100

    feat/NPIRA-0006_AddOGMetaData

---
+++ app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Nigerian Property Investment Risk Analyzer",
+    description: "Comprehensive analysis and risk assessment tools for Nigerian diasporans looking to invest in homeland property.",
+  };
